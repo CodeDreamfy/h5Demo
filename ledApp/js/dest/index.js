2c42294d5b00b83462393c0d0f02298b20f53198
codedreamfy <codedreamfy@outlook.com>
2016-12-16 23:38:05 +0800

    led

--- ledApp/js/dest/index.js
+++ ledApp/js/dest/index.js
@@ -1,109 +1,267 @@
 'use strict';
 
-$(function () {
-		uiRange.init();
-		var shadowVal = 10;
-		var itemW = void 0,
-		    $sceneList = void 0;
+var swiper = void 0,
+    COLOR = "#fff",
+    Light = 50,
+    ColorPick = void 0,
+    swIndex = void 0;
 
-		$('.led-wrapper').on('touchstart', '.conent-main .ledwrap', function (e) {
-				var flag = $(this).data('flag');
-				if (!flag) {
-						setLedShadow(shadowVal);
-						$(this).data('flag', true);
-				} else {
-						setLedShadow(shadowVal, '#10042e');
-						$(this).data('flag', false);
-				}
-				console.log($(this).data('flag'));
-				$('.nav-wrap').toggleClass('active');
-				$('.conent-main .sensor-wrapper').toggleClass('hide');
-				e.preventDefault();
-		});
-
-		$('.led-wrapper').on('touchstart', '.nav-wrap .nav-inner .nav-item', function (e) {
-				e.preventDefault();
-				if ($(this).index() == 3) return false;
-				if ($(this).index() == 2) {
-						$('.control .scene-wrap').show();
-						$('.control').removeClass('show').addClass('show-scene');
-						if (!itemW) {
-								itemW = $('.control .scene-compute .scene-item').width();
-								$sceneList = $('.control .scene-wrap .scene-list');
-						}
-				} else {
-						$('.control .scene-wrap').hide();
-						$('.control .content').removeClass('hide');
-				};
-
-				$(this).addClass('active').siblings().removeClass('active');
-				$('.control').addClass('show');
-		});
-
-		$('.led-wrapper').on('touchstart', '.control .arrow', function (e) {
-				e.preventDefault();
-				$('.control').toggleClass('show');
-		});
-
-		$('.led-wrapper').on('touchstart', '.control .scene-wrap .scene-item', function (e) {
-				$(this).addClass('active').siblings().removeClass('active');
-				$('.control').removeClass("show-scene");
-				e.preventDefault();
-		});
-		$('.led-wrapper').on('touchstart', '.control .scene-wrap .prev', function (e) {
-				var l = $sceneList.position().left;
-				console.log(Math.abs(l));
-				if (Math.abs(l) <= itemW * 3) {
-						var c = l + itemW;
-						$sceneList.css('left', c);
-						console.log(Math.abs($sceneList.position().left));
-						$(this).siblings('.next').show();
-				}
-				if ($sceneList.position().left >= 0) $(this).hide();
-		});
-		$('.led-wrapper').on('touchstart', '.control .scene-wrap .next', function (e) {
-				var l = $sceneList.position().left;
-				if (Math.abs(l) < itemW * 3) {
-						var c = l - itemW;
-						$sceneList.css('left', c);
-						$(this).siblings('.prev').show();
-				}
-				if (Math.abs($sceneList.position().left) >= itemW * 3) $(this).hide();
-		});
+//app ready
+OJS.bindAppReady(function () {
+  notNetwork();
+  OJS.bindReady(function () {
+    bindEvent();
+    var initData = OJS.device.getSensorData();
+    if (OJS.device.onlineStatus == 1) {
+      $('.device-status em').addClass('online');
+    } else {
+      $('.device-status em').addClass('offline');
+    }
+    OJS.device.bindPushData({
+      'deviceStatusChange': function deviceStatusChange(data) {
+        if (OJS.device.onlineStatus == 1) {
+          setScene(data);
+        } else {
+          OJS.app.toast("设备不在线，无法更新数据！");
+        }
+      }
+    });
+  });
 });
+//send
+function send(o) {
+  var ops = {};
+  var defaults = {
+    'hues': null,
+    'saturation': null,
+    'brightness': null,
+    'DcMotor': null
+  };
+  ops = $.extend({}, defaults, o);
+  var result = OJS.device.sendNotify(ops, function () {
+    OJS.app.toast('命令已经下发');
+  }, function () {
+    OJS.app.toast('设备已经收到命令！');
+  });
+  if (!result) {
+    OJS.app.toast('命令发送失败，无法连接到服务器');
+  }
+}
 
-function setLedShadow(val, color) {
-		var $round = $('.conent-main .ledwrap');
-		var defaultColor = !!color ? color : '#fff';
-		var opacity = val / 100;
-		$round.find('.r1').css({
-				'box-shadow': '0 0 ' + val * 0.55 + 'px 0 ' + defaultColor,
-				'background': defaultColor
-		});
-		$('.r2,.r3', $round).css({
-				'box-shadow': '0 0 ' + val * 0.6 + 'px 0 ' + defaultColor,
-				'background': defaultColor
-		});
+//无网络判断
+function notNetwork() {
+  setInterval(function () {
+    OJS.app.hasNetWork(function (data) {
+      if (data) {
+        OJS.ui.hideOfflineMask();
+        // OJS.app.toast(data+'有网络');
+      } else {
+        OJS.ui.showOfflineMask();
+        // OJS.app.toast(data+'无网络');
+      }
+    });
+  }, 5000);
+}
+
+//set index data
+function setScene(data) {
+  var $sensorWrapper = $('.sensor-wrapper');
+  $sensorWrapper.find('.temp .val em').text(data.temperature);
+  $sensorWrapper.find('.humidity .val em').text(data.humidity);
+  $sensorWrapper.find('.intensity .val em').text(data.LightSensor);
+  $sensorWrapper.find('.distance .val em').text(data.infrared);
+
+  //电机开关
+  if (data.DcMotor) {
+    if (!$('[for="label-led1"]').prev().prop('checked')) {
+      $('[for="label-led1"]').click();
+    }
+  } else {
+    if ($('[for="label-led1"]').prev().prop('checked')) {
+      $('[for="label-led1"]').click();
+    }
+  }
+}
+
+function bindEvent() {
+  $('.led-wrapper').on('touchstart', '.dianji', function (e) {
+    var flag = void 0;
+    if ($('[for="label-led1"]').prev().prop('checked')) {
+      flag = false;
+    } else {
+      flag = true;
+    }
+    send({ 'DcMotor': { boolean: flag } });
+  });
+
+  $('.led-wrapper').on('touchstart', '.conent-main .ledwrap', function (e) {
+    var flag = $(this).data('flag');
+    if (!flag) {
+      setLight(50, '#fff', '#fff');
+      $(this).data('flag', true);
+      send({ 'brightness': { float: 50 } });
+      $('.nav-wrap').addClass('active');
+    } else {
+      setLight(0, '#10042e', '#10042e');
+      $(this).data('flag', false);
+      $('.control').removeClass('show-scene show');
+      $('.nav-inner .nav-item').removeClass('active');
+      $('.led-canvas').hide();
+      // console.log("off")
+      send({ 'brightness': { float: 0 } });
+      $('.nav-wrap').removeClass('active');
+    }
+
+    $('.conent-main .sensor-wrapper').toggleClass('hide');
+    e.preventDefault();
+  });
+
+  $('.led-wrapper').on('touchstart', '.nav-wrap .nav-inner .nav-item', function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    var oData = OJS.device.getSensorData();
+    var index = $(this).index();
+    $('.mingdu').val(50).prev().css('width', '50%');
+    $('.baohe').val(50).prev().css('width', '50%');
+    if (index == 3) return false;
+    if (index == 0) {
+      send({ 'brightness': { float: 50 } });
+      setLight(50, '#fff', '#fff');
+    }
+    if (index == 1) {
+      $('.led-canvas').show();
+      if (!ColorPick) ColorPick = $('.led-canvas').ColorPicker();
+      //console.log($('.led-canvas').data('colorHsl'))
+      setLight(50, $('.led-canvas').data('colorHsl'), '#fff');
+    } else {
+      $('.led-canvas').hide();
+    }
+    if (index == 2) {
+      $('.control .scene-wrap').show();
+      $('.control').removeClass('show').addClass('show-scene');
+      setLight(50, '#fff', '#fff');
+      if (!swiper) {
+        swiper = new Swiper('.swiper-container', {
+          slidesPerView: 5,
+          setWrapperSize: true,
+          prevButton: '.swiper-button-prev',
+          nextButton: '.swiper-button-next',
+          onTap: function onTap(sw) {
+            console.log(sw.clickedIndex);
+            if ($(sw.clickedSlide).hasClass('active')) {
+              $(sw.clickedSlide).removeClass('active');
+              $('.control').addClass("show-scene");
+              send({
+                'hues': { float: 0 }
+              });
+            } else {
+              $('.swiper-wrapper .swiper-slide').eq(sw.clickedIndex).addClass('active').siblings().removeClass('active');
+              swIndex = $('.swiper-wrapper .swiper-slide').eq(sw.clickedIndex);
+              $('.control').removeClass("show-scene");
+              send({
+                'saturation': { float: parseInt($('.baohe').val()) },
+                'brightness': { float: parseInt($('.mingdu').val()) },
+                'hues': { float: parseInt(swIndex.data('hsl')) }
+              });
+              //console.log(swIndex.data('hsl'), $('.mingdu').val(), $('.baohe').val())
+            }
+            $('.mingdu').val(50).prev().css('width', '50%');
+            $('.baohe').val(50).prev().css('width', '50%');
+          }
+        });
+      }
+    } else {
+      $('.control .scene-wrap').hide();
+      $('.control .content').removeClass('hide');
+    };
+    $(this).addClass('active').siblings().removeClass('active');
+    $('.control').addClass('show');
+    $('.nav-wrap').removeClass('active');
+  });
+
+  $('.led-wrapper').on('touchstart', '.control .arrow', function (e) {
+    $('.control').removeClass('show');
+    if ($('.control').hasClass('show-scene')) $('.control').removeClass("show-scene");
+    $('.nav-wrap').addClass('active');
+    e.preventDefault();
+  });
+
+  $('.led-wrapper').on('touchend', '.control .scene-wrap .scene-item', function (e) {
+    e.stopPropagation();
+    //console.log(e.target.className)
+    $(this).addClass('active').siblings().removeClass('active');
+    $('.control').removeClass("show-scene");
+    e.preventDefault();
+    return false;
+  });
+}
+
+function setLight(val, color, bg) {
+  var $round = $('.conent-main .ledwrap');
+  if (val == 0) bg = '#10042e';
+  $round.find('.r1').css({
+    'box-shadow': '0 0 ' + val * 0.75 + 'px 0 ' + color,
+    'background': bg
+  });
+  $('.r2,.r3', $round).css({
+    'box-shadow': '0 0 ' + val * 0.6 + 'px 0 ' + color,
+    'background': bg
+  });
 }
 
 var uiRange = {
-		init: function init() {
-				$('.ui-range').forEach(function (elem, index) {
-						var $input = $(elem).find('input[type=range]');
-						var min = $input.attr('min');
-						var max = $input.attr('max');
-						var now = $input.val();
-						var val = (now - min) / (max - min) * 100;
-						$(elem).find('.progress').css({
-								width: val + '%'
-						});
-						$input.on('input change', function () {
-								var n = $(this).val();
-								$(this).siblings('.progress').css({
-										width: n + '%'
-								});
-								setLedShadow(n);
-						});
-				});
-		}
+  init: function init() {
+    $('.ui-range').forEach(function (elem, index) {
+      var $input = $(elem).find('input[type=range]');
+      var min = $input.attr('min');
+      var max = $input.attr('max');
+      var now = $input.val();
+      var val = (now - min) / (max - min) * 100;
+      $(elem).find('.progress').css({
+        width: val + '%'
+      });
+      $input.on('input change', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        var n = $(this).val();
+        $(this).siblings('.progress').css({
+          width: n + '%'
+        });
+
+        var index = $('.nav-inner').children('.active').index();
+        if (index == 0) {
+          if ($(this).hasClass('mingdu')) {
+            setLight(n, '#fff', '#fff');
+          }
+          send({
+            'saturation': { float: parseInt($('.baohe').val()) },
+            'brightness': { float: parseInt($('.mingdu').val()) }
+          });
+          // console.log("index:",index,':',$('.baohe').val(), $('.mingdu').val())
+        } else if (index == 1) {
+          if ($(this).hasClass('mingdu')) {
+            setLight(n, $('.led-canvas').data('colorHsl'), '#fff');
+          }
+          send({
+            'saturation': { float: parseInt($('.baohe').val()) },
+            'brightness': { float: parseInt($('.mingdu').val()) },
+            'hues': { float: $('.led-canvas').data('color') }
+          });
+          // console.log("index:",index,':',$('.led-canvas').data('color'),$('.baohe').val(), $('.mingdu').val())
+        } else if (index == 2) {
+          if ($(this).hasClass('mingdu')) {
+            setLight(n, '#fff', '#fff');
+          }
+          send({
+            'saturation': { float: parseInt($('.baohe').val()) },
+            'brightness': { float: parseInt($('.mingdu').val()) },
+            'hues': { float: swIndex.data('hsl') }
+          });
+          // console.log("index:",index,':',swIndex.data('hsl'),$('.baohe').val(), $('.mingdu').val())
+        }
+      });
+    });
+  }
 };
+
+uiRange.init();
---
+++ ledApp/js/src/canvasPicker.js
@@ -0,0 +1,195 @@
+let ColorPicker = function(elem, option){
+  this.author = 'CodeDreamfy';
+  this.version = '0.0.1';
+  this.$wrap = $(elem);
+  this.defaultOption = {
+    placeWrap: '.c-place',
+    fonts: null
+  }
+  this.options = $.extend({}, this.defaultOption, option || {})
+
+  //init
+  this._init();
+  return this;
+}
+$.extend(ColorPicker.prototype, {
+  _init: function(){
+    this.canvasElem = $('<canvas>').get(0);
+    this.canvasElem.id = 'canvasPick';
+    this.context = this.canvasElem.getContext('2d');
+    this.canvas = {
+      WIDTH: this.$wrap.width(),
+      HEIGHT: this.$wrap.height()
+    };
+    this.canvas.RadiusX = this.canvas.WIDTH/2;
+    this.canvas.RadiusY = this.canvas.HEIGHT/2;
+    this.canvas.MaxRadius = this.canvas.RadiusX;
+    this.canvas.MinRadius = this.$wrap.find(this.options.placeWrap).width()/2;
+    /**设置canvas元素宽高 */
+    this.canvasElem.width = this.canvas.WIDTH;
+    this.canvasElem.height = this.canvas.HEIGHT;
+    /**小圆 */
+    this.canvas.mouseR = (this.canvas.MaxRadius - this.canvas.MinRadius)/2;
+    this.canvas.mouseC = (this.canvas.MaxRadius + this.canvas.MinRadius)/2;
+    this.canvas.mouseLx = this.canvas.MaxRadius;
+
+
+    this._render();
+    this.$wrap.data('color', this.turnHsl());
+    this.$wrap.data('colorHsl', this.nowColor);
+    this._bindEvent();
+    // this.getRgba(this.canvas.MaxRadius,this.canvas.MaxRadius-this.canvas.MinRadius-20);
+    this.$wrap.append(this.canvasElem);
+    this.canvas.offset = this.$wrap.offset();
+
+  },
+  _render: function(){
+    this._roundbg();
+    this._mousePick();
+  },
+  _roundbg: function(){
+    this.context.save();
+    let alpha = 0,
+        i = 0.2,
+        rad = i * Math.PI / 180,
+        k = 0;
+    this.context.translate(this.canvas.RadiusX,this.canvas.RadiusY);
+    for(; alpha < 720; alpha += i){
+      k+=1;
+      if(k <= 14 ){
+        this.context.strokeStyle = 'hsla('+ alpha +', 100%, 50%, 0)';
+      }else {
+        this.context.strokeStyle = 'hsla('+ alpha +', 100%, 50%, 1.0)';
+        if(k==120) k=0;
+      }
+      this.context.rotate(rad);
+      this.context.beginPath();
+      this.context.moveTo(this.canvas.MinRadius+6, 0);
+      this.context.lineTo(this.canvas.MaxRadius-6,0);
+      this.context.stroke();
+    }
+    this.cpRound = this.context.getImageData(0, 0, this.canvas.WIDTH, this.canvas.HEIGHT);
+    this.context.restore();
+    this.context.translate(this.canvas.RadiusX,this.canvas.RadiusY);
+  },
+  _mousePick: function(x,y){
+    this.context.save();
+    let color,k,beta, seta;
+    if(arguments.length == 0){
+      seta = 0;
+    }else {
+      beta = Math.atan2(y-this.canvas.RadiusY,x-this.canvas.RadiusX);
+      this.context.rotate(beta);
+      this.context.beginPath();
+      beta < 0 && (beta += 2*Math.PI);
+      seta = beta*360/(2*Math.PI);
+    }
+    this.context.fillStyle = this.context.strokeStyle = 'hsla('+ seta +', 100%, 50%, 1.0)';
+    this.nowColor = this.context.strokeStyle = 'hsla('+ seta +', 100%, 50%, 1.0)';
+    this.context.strokeStyle = "#fff";
+    this.context.lineWidth = 2;
+    this.context.lineJoin = 'round';
+    this.context.arc(this.canvas.mouseC, 0, this.canvas.mouseR-1, 0, 2*Math.PI);
+    this.context.stroke();
+    this.context.fill();
+    this.context.restore();
+  },
+  _redrew: function(x,y){
+    this.context.clearRect(0, 0, this.canvas.WIDTH, this.canvas.HEIGHT);
+    this.context.putImageData(this.cpRound, 0, 0);
+    this.context.save();
+    this._mousePick(x,y);
+    this.context.restore();
+  },
+  _bindEvent: function(){
+    let that = this,x,y;
+    // console.log(that.canvas.offset)
+    this.canvasElem.addEventListener('touchstart', function(event){
+      event.stopPropagation();
+      x = event.touches[0].clientX-that.canvas.offset.left;
+      y = event.touches[0].clientY-that.canvas.offset.top;
+      that._redrew(x,y);
+      // return false;
+      
+    })
+    this.canvasElem.addEventListener('touchmove', function(event){
+      event.stopPropagation();
+      x = event.touches[0].clientX-that.canvas.offset.left;
+      y = event.touches[0].clientY-that.canvas.offset.top;
+      that._redrew(x,y);
+      // return false;
+    })
+    this.canvasElem.addEventListener('touchend', function(event){
+      x = event.changedTouches[0].clientX-that.canvas.offset.left;
+      y = event.changedTouches[0].clientY-that.canvas.offset.top;
+      that._redrew(x,y);
+      that.$wrap.data('color', that.turnHsl());
+      that.$wrap.data('colorHsl', that.nowColor);
+      
+      setLight($('.mingdu').val(),that.nowColor,'#fff')
+      send({
+        'saturation': {float: parseInt($('.baohe').val())},
+        'brightness': {float: parseInt($('.mingdu').val())},
+        'hues': {float: parseInt(that.turnHsl())}
+      })
+      // console.log('colorP',that.turnHsl())
+      event.stopPropagation();
+      // return false;
+    })
+  },
+  getRgba: function(x,y){
+    let rgb = this.context.getImageData(x,y,1,1);
+    let data = rgb.data;
+    return 'rgba('+data[0]+','+data[1]+','+data[2]+',1)'
+  },
+  getHsl: function(r,g,b){
+    let max = Math.max(r, g, b), min = Math.min(r, g, b);
+    let h;
+    if(max == min){
+        h = 0; // achromatic
+    }else{
+        let d = max - min;
+        switch(max){
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+        }
+        h /= 6;
+    }
+
+    return h;
+  },
+  getRgba: function(){
+    var r, g, b;
+    if(s == 0){
+        r = g = b = l; // achromatic
+    }else{
+        var hue2rgb = function hue2rgb(p, q, t){
+            if(t < 0) t += 1;
+            if(t > 1) t -= 1;
+            if(t < 1/6) return p + (q - p) * 6 * t;
+            if(t < 1/2) return q;
+            if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+            return p;
+        }
+
+        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+        var p = 2 * l - q;
+        r = hue2rgb(p, q, h + 1/3);
+        g = hue2rgb(p, q, h);
+        b = hue2rgb(p, q, h - 1/3);
+    }
+
+    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+  },
+  turnHsl: function(){
+    let m = this.nowColor;
+    m = m.slice(5).split(',')[0]
+    return (+m).toFixed(2)
+  }
+
+})
+
+$.fn.ColorPicker = function(option) {
+  return new ColorPicker(this, option);
+}
